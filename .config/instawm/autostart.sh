7dedb708e4cdf41f20b6435a8beb6eef75914c40
The-Repo-Club <wayne6324@gmail.com>
2021-04-12 17:53:50 +0100

    update configs

---
+++ .config/instawm/autostart.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# path:       ~/.config/dwm/autostart.sh
+# author:     The-Repo-Club [wayne6324@gmail.com]
+# github:     https://github.com/The-Repo-Club/instawm
+# date:       2021-04-10 12:34:47.440579
+#
+
+# if ! command -v autorandr &> /dev/null; then
+#     echo "autorandr could not be found"
+#     exit
+# else
+#     autorandr --force --load instawm &
+# fi
+
+# if ! command -v xsettingsd &> /dev/null; then
+#     echo "xsettingsd could not be found"
+#     exit
+# else
+#     xsettingsd &
+# fi
+
+# if ! command -v polkit-dumb-agent &> /dev/null; then
+#     echo "polkit-dumb-agent could not be found"
+#     exit
+# else
+#     polkit-dumb-agent &
+# fi
+
+# if ! command -v xautolock &> /dev/null; then
+#     echo "xautolock could not be found"
+#     exit
+# else
+#     xautolock -time 10 -locker 'multimonitorlock -l -- --timestr="%H:%M"' -notify 30 -notifier "notify-send -u critical -- 'LOCKING screen in 30 seconds'" &
+# fi
+
+# if ! command -v nm-applet &> /dev/null; then
+#     echo "nm-applet could not be found"
+#     exit
+# else
+#     sleep 2s && nm-applet &
+# fi
